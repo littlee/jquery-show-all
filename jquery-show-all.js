a9dbe020f60c63ca08f8da9781e77a808d0efc54
littlee <usedlittlee@gmail.com>
2017-11-30 01:22:19 +0800

    add resize event handler

--- jquery-show-all.js
+++ jquery-show-all.js
@@ -1,10 +1,28 @@
 ;(function($) {
-
   function supportFlexbox() {
     var d = document.createElement('div')
     d.style.display = 'flex'
     return d.style.display === 'flex'
   }
+
+  var bindElem = []
+
+  window.addEventListener('resize', () => {
+    bindElem.forEach(function(item) {
+      item.elem.each(function() {
+        var $this = $(this)
+        var $parent = $this.parent()
+        var parentWidth = $parent.width()
+        var parentHeight = $parent.height()
+        var parentRatio = parentWidth / parentHeight
+        if (parentRatio > item.elemRatio) {
+          $this.height(parentHeight).width(parentHeight * item.elemRatio)
+        } else {
+          $this.width(parentWidth).height(parentWidth / item.elemRatio)
+        }
+      })
+    })
+  }, false)
 
   $.fn.showAll = function(rWidth, rHeight, options) {
     if (typeof rWidth !== 'number' || rWidth <= 0) {
@@ -23,8 +41,16 @@
     if (options) {
       options = $.extend({}, defaultOptions, options)
     }
+    else {
+      options = defaultOptions
+    }
 
     var elemRatio = rWidth / rHeight
+
+    bindElem.push({
+      elemRatio: elemRatio,
+      elem: this
+    })
 
     return this.each(function() {
       var $this = $(this)
@@ -39,19 +65,43 @@
       // take all parent height
       if (parentRatio > elemRatio) {
         $this.height(parentHeight).width(parentHeight * elemRatio)
-      }
-      // take all parent width
-      else {
+      } else {
+        // take all parent width
         $this.width(parentWidth).height(parentWidth / elemRatio)
       }
 
+      var styles = {}
+
       if (supportFlexbox()) {
-        $parent.css('display', 'flex')
+        styles.display = 'flex'
+        styles.flexDirection = 'row'
         if (options.horizontalAlign) {
+          styles.justifyContent = 'center'
         }
+        if (options.verticalAlign) {
+          styles.alignItems = 'center'
+        }
+        $parent.css(styles)
+      } else {
+        styles.margin = 'auto'
+        if (options.horizontalAlign) {
+          styles.left = 0
+          styles.right = 0
+        }
+
+        if (options.verticalAlign) {
+          styles.top = 0
+          styles.bottom = 0
+        }
+        $this.css(styles)
       }
 
       return this
     })
   }
+
+  $('[data-show-all]').each(function() {
+    var $this = $(this)
+    $this.showAll($this.data('width'), $this.data('height'))
+  })
 })(jQuery)
